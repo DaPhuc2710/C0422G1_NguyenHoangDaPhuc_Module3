a633a6f58ae6643ad6525e0c76e86e1b7208578d
DaPhuc2710 <daphuc2000@gmail.com>
2022-07-14 09:02:53 +0700

    1111

--- ss1.Database/Homework/Practice.sql
+++ ss1.Database/Homework/Practice.sql
@@ -1,19 +1,29 @@
-use student_management;
-create table class(
-id int auto_increment,
-name varchar(45),
-primary key(id)
+DROP DATABASE IF EXISTS student_management;
+CREATE DATABASE student_management;
+USE student_management;
+CREATE TABLE class (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(45),
+    PRIMARY KEY (id)
 );
-use class;
-insert into class(name) values ("Hà Duyên"),("Đa Phúc"),("Hoàng Thái"),("Võ Vinh"),("Hoàng Long");
-select * from class;
-create table teacher(
-id int auto_increment,
-name varchar(45),
-age int,
-country varchar(45),
-primary key(id)
+
+INSERT INTO class(name) VALUES ("Hà Duyên"),("Đa Phúc"),("HOÀng Thái"),("Võ VInh"),("HoàNG Long");
+-- hiỂn thị tất cả trong class
+select * from clASS;
+-- cHỈ Hiển thị id trong class
+select id from class;
+
+CREATE TABLE teacher (
+-- CHạY AUTo id tự đỘng tăng
+    id INT AUTO_INCREMENT,
+    name VARCHAR(45),
+    AGE INT,
+    COUNTRY VARCHAR(45),
+    PRIMARY KEY (ID)
 );
-insert into teacher(name,age,country) values("Nguyễn Văn A",30,"Việt Nam"),("Nguyễn Văn B",40,"Trung Quốc"),("James Smith",30,"America");
-select * from teacher; 
+INSERT INTO teacher(name,age,country) values("NguYễn VĂn A",30,"ViỆt NAM"),("Nguyễn Văn B",40,"Trung Quốc"),("James Smith",30,"AMERIca");
+SELECT * FROM teacher; 
+
+-- hiển thị KhôNG TRÙnG LẬP
+select DISTINCT name FROM teacher;
 
