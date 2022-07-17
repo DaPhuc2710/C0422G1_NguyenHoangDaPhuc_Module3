97f5af8b42713be1cb86e3372e8174a1aeb55db1
DaPhuc2710 <daphuc2000@gmail.com>
2022-07-17 17:12:32 +0700

    111

--- Case_study/task1-2.sql
+++ Case_study/task1-2.sql
@@ -119,6 +119,7 @@ CREATE TABLE hop_dong_chi_tiet (
         REFERENCES case_study.dich_vu_di_kem (ma_dich_vu_di_kem)
 );
 
+-- Câu 1 Thêm mới thông tin cho tất cả các bảng có trong CSDL -- 
 insert into vi_tri(ten_vi_tri) 
 values("Quản lý"),("Nhân viên");
 
@@ -206,6 +207,76 @@ values
 (1,1,3),
 (2,1,2),
 (2,12,2);
+
+--  Câu 2 Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự. --
+select 
+nv.ho_ten,
+nv.ngay_sinh,
+nv.so_cmnd,
+nv.luong,
+nv.so_dien_thoai,
+nv.email,
+nv.dia_chi,
+vt.ten_vi_tri,
+td.ten_trinh_do,
+bp.ten_bo_phan
+from nhan_vien nv 
+join vi_tri vt on nv.ma_vi_tri = vt.ma_vi_tri
+join trinh_do td on nv.ma_trinh_do = td.ma_trinh_do
+join bo_phan bp on  nv.ma_bo_phan = bp.ma_bo_phan
+where nv.ho_ten regexp "^(\\S+(\\s\\S+)*(\\s(H|T|K)\\S+)+)$" 
+and char_length(nv.ho_ten)<=15;
+
+-- Câu 3 Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+select 
+kh.ho_ten,
+kh.ngay_sinh,
+kh.gioi_tinh,
+kh.so_cmnd,
+kh.so_dien_thoai,
+kh.email,
+kh.dia_chi, 
+lk.ten_loai_khach
+from khach_hang kh
+join loai_khach lk
+on kh.ma_loai_khach = lk.ma_loai_khach
+where kh.dia_chi like "% Đà Nẵng" or kh.dia_chi like "% Quảng Trị" and (year(current_date())-year(kh.ngay_sinh)) between 18 and 50;
+
+-- Câu 4.Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
+-- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+select count(*) as so_lan_dat,
+kh.ho_ten
+from 
+khach_hang kh 
+join
+loai_khach lk on lk.ma_loai_khach=kh.ma_loai_khach
+join 
+hop_dong hd on kh.ma_khach_hang =hd.ma_khach_hang 
+where lk.ten_loai_khach like "Diamond"    
+group by kh.ma_khach_hang
+order by so_lan_dat asc ; 
+
+-- Câu 5.Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
+-- (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet)  
+-- cho tất cả các khách hàng đã từng đặt phòng. 
+-- (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+select 
+kh.ma_khach_hang,
+kh.ho_ten,
+lk.ten_loai_khach,
+hd.ma_hop_dong,
+dv.ten_dich_vu,
+hd.ngay_lam_hop_dong,
+hd.ngay_ket_thuc,
+ sum(chi_phi_thue + so_luong*gia) as tong_tien 
+ from khach_hang kh 
+ join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+ join hop_dong hd on kh.ma_khach_hang =hd.ma_khach_hang
+ join dich_vu dv on dv.ma_dich_vu= hd.ma_dich_vu
+ join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+ join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem=dvdk.ma_dich_vu_di_kem;
+
+
 
 
 
