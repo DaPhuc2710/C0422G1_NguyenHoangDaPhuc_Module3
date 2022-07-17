c598fe223a187d081863934560308e451ced2206
DaPhuc2710 <daphuc2000@gmail.com>
2022-07-17 17:36:26 +0700

    11

--- Case_study/task1-2.sql
+++ Case_study/task1-2.sql
@@ -268,13 +268,20 @@ hd.ma_hop_dong,
 dv.ten_dich_vu,
 hd.ngay_lam_hop_dong,
 hd.ngay_ket_thuc,
- sum(chi_phi_thue + so_luong*gia) as tong_tien 
- from khach_hang kh 
- join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
- join hop_dong hd on kh.ma_khach_hang =hd.ma_khach_hang
- join dich_vu dv on dv.ma_dich_vu= hd.ma_dich_vu
- join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
- join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem=dvdk.ma_dich_vu_di_kem;
+sum(dv.chi_phi_thue + hdct.so_luong*dvdk.gia) as tong_tien 
+from khach_hang kh 
+join loai_khach lk 
+on kh.ma_loai_khach = lk.ma_loai_khach
+join  hop_dong hd 
+on kh.ma_khach_hang =hd.ma_khach_hang
+join dich_vu dv 
+on  hd.ma_dich_vu = dv.ma_dich_vu
+join hop_dong_chi_tiet hdct 
+on  hd.ma_hop_dong = hdct.ma_hop_dong 
+join dich_vu_di_kem dvdk 
+on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by kh.ma_khach_hang
+order by kh.ma_khach_hang;
 
 
 
