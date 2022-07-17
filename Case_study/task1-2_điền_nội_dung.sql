611e220462e2ef9fc99254a1ef80b42a5fa10094
DaPhuc2710 <daphuc2000@gmail.com>
2022-07-17 13:40:42 +0700

    111

--- Case_study/task1-2_điền_nội_dung.sql
+++ Case_study/task1-2_điền_nội_dung.sql
@@ -1,42 +1,10 @@
-use case_study;
-insert into vi_tri(ten_vi_tri) 
-values("Quản lý","Nhân viên");
 
-insert into trinh_do(ten_trinh_do) 
-values("Trung cấp","Cao đẳng","Đại học", "Sau đại học");
 
-insert into bo_phan(ten_bo_phan) 
-values("Sale-Marketing","Hành chính","Phục vụ","Quản lý");
 
-insert into loai_khach(ten_loai_khach)
- values ("Diamond","Platinium","Gold","Silver","Member");
- 
-insert into loai_dich_vu (ten_loai_dich_vu) 
-values("Villa", "House","Room");
 
-insert into kieu_thue (ten_kieu_thue) 
-values ("year","month","day","hour");
+	
 
-insert into dich_vu_di_kem(ten_dich_vu_di_kem,gia,don_vi,trang_thai) 
-values
-("Karaoke","10000","giờ","tiện nghi, hiện đại"),
-("Thuê xe máy","10000","chiếc","hỏng 1 chiếc"),
-("Thuê xe đạp","20000","chiếc","tốt"),
-("Buffer buổi sáng","15000","suất","đầy đủ đồ ăn tráng miệng"),
-("Buffer buổi trưa","90000","suất","đầy đủ đồ ăn tráng miệng"),
-("Buffer buổi tối","16000","suất","đầy đủ đồ ăn tráng miệng");
-insert into nhan_vien(ho_ten,ngay_sinh,so_cmnd,luong,so_dien_thoai,email,dia_chi,ma_vi_tri,ma_trinh_do,ma_bo_phan,ma_vi_tri,ma_trinh_do,ma_bo_phan)
-values
-("Nguyễn Văn An","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-("Lê Văn Bình","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-("Nguyễn Văn An","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-("Nguyễn Văn An","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-("Nguyễn Văn An","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-("Nguyễn Văn An","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-("Nguyễn Văn An","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-("Nguyễn Văn An","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-("Nguyễn Văn An","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-("Nguyễn Văn An","1970-11-07","456231786","10000000","0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
+
 
 
 
