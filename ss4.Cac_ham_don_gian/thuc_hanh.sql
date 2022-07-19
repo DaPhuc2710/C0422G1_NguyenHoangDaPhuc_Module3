4da7a6b78fb63e0583b6c697ba8ae0fe750f62ce
DaPhuc2710 <daphuc2000@gmail.com>
2022-07-19 08:23:23 +0700

    111

--- ss4.Cac_ham_don_gian/thuc_hanh.sql
+++ ss4.Cac_ham_don_gian/thuc_hanh.sql
@@ -32,12 +32,12 @@ CREATE TABLE student (
  ("Hoa","Hải Phòng",null,1,1),
  ("mạnh","HCM","0123123123",0,2);
  
- create table `subject`(
- sub_ID int auto_increment primary key,
- sub_name varchar(20),
- credit int,
- `status` INT
- );
+CREATE TABLE `subject` (
+    sub_ID INT AUTO_INCREMENT PRIMARY KEY,
+    sub_name VARCHAR(20),
+    credit INT,
+    `status` INT
+);
  
  insert into `subject` (sub_name,credit,`status`) 
  values("Cf",5,1),
@@ -45,15 +45,17 @@ CREATE TABLE student (
  ("hdj",5,1),
  ("rdbms",10,1);
  
- creaTE TAble mark(
- mark_Id int primary key auto_increment,
- sub_id int NOT Null ,
- student_ID int NOT Null ,
- mark_point int ,
- examtimes int,
- foreign key (sub_id) references `subject`(sub_ID),
- foreign key (student_iD) references student(student_iD)
- );
+CREATE TABLE mark (
+    mark_Id INT PRIMARY KEY AUTO_INCREMENT,
+    sub_id INT NOT NULL,
+    student_ID INT NOT NULL,
+    mark_point INT,
+    examtimes INT,
+    FOREIGN KEY (sub_id)
+        REFERENCES `subject` (sub_ID),
+    FOREIGN KEY (student_iD)
+        REFERENCES student (student_iD)
+);
  
  insert into mark (sub_ID,student_iD,mark_point,examtimes) 
  values(1,1,8,1),
@@ -61,58 +63,88 @@ CREATE TABLE student (
  (2,1,12,1);
  -- Phần thực hành
   -- Hiển thị số lượng sinh viên ở từng nơi
-  select address,
-  count(student_ID) as so_luong_hoc_vien
-  from student
-  group by address;
+  SELECT 
+    address, COUNT(student_ID) AS so_luong_hoc_vien
+FROM
+    student
+GROUP BY address;
   -- Tính điểm trung bình các môn học của mỗi học viên
-  select 
-  s.student_ID,
-  s.student_name,
-  ifnull(avg(mark_point),0) as diem_trung_binh
-  from student s 
-  left join mark m 
-  on s.student_ID = m.student_ID
-  group by s.student_name;
+ SELECT 
+    s.student_ID,
+    s.student_name,
+    IFNULL(AVG(mark_point), 0) AS diem_trung_binh
+FROM
+    student s
+        LEFT JOIN
+    mark m ON s.student_ID = m.student_ID
+GROUP BY s.student_name;
   -- Hiển thị những bạn học viên co điểm trung bình các môn học lớn hơn 15
-  select s.student_ID,
-  s.student_name,
-  avg(mark_point)
-  from student s 
-  inner join mark m 
-  on s.student_ID = m.student_ID
-  group by s.student_ID
-  having avg(mark_point)>15;
+  SELECT 
+    s.student_ID, s.student_name, AVG(mark_point)
+FROM
+    student s
+        INNER JOIN
+    mark m ON s.student_ID = m.student_ID
+GROUP BY s.student_ID
+HAVING AVG(mark_point) > 15;
   -- Hiển thị thông tin các học viên có điểm trung bình lớn nhất.
-  select s.student_ID,
-  s.student_name,
-  avg(mark_point)
-  from student s 
-  inner join mark m 
-  on s.student_ID = m.student_ID
-  group by s.student_ID,s.student_name
-  having avg(mark_point) >= all(select avg(mark_point) from mark group by student_ID);
+  SELECT 
+    s.student_ID, s.student_name, AVG(mark_point)
+FROM
+    student s
+        INNER JOIN
+    mark m ON s.student_ID = m.student_ID
+GROUP BY s.student_ID , s.student_name
+HAVING AVG(mark_point) >= ALL (SELECT 
+        AVG(mark_point)
+    FROM
+        mark
+    GROUP BY student_ID);
   
   -- Phần bài tập
   -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
-  select *
-  from `subject`sub
-  where sub.credit = (select max(sub.credit) from `subject`sub);
+ SELECT 
+    *
+FROM
+    `subject` sub
+WHERE
+    sub.credit = (SELECT 
+            MAX(sub.credit)
+        FROM
+            `subject` sub);
   
   -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
-  select sub.sub_ID ,sub.sub_name,sub.credit ,sub.`status`,  m.mark_point as diem_thi_lon_nhat
-  from `subject` sub
-  inner join mark m 
-  on sub.sub_ID = m.sub_ID
-  where m.mark_point = (select max(mark_point) from mark);
+ SELECT 
+    sub.sub_ID,
+    sub.sub_name,
+    sub.credit,
+    sub.`status`,
+    m.mark_point AS diem_thi_lon_nhat
+FROM
+    `subject` sub
+        INNER JOIN
+    mark m ON sub.sub_ID = m.sub_ID
+WHERE
+    m.mark_point = (SELECT 
+            MAX(mark_point)
+        FROM
+            mark);
   
   -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-  select  s.student_ID, s.student_name, s.address, s.phone, s.`status`, s.class_ID, avg(m.mark_point) as diem_thi_trung_binh
-  from student s 
-  inner join mark m  
-  on s.student_ID = m.student_ID
-  group by s.student_ID
-  order by avg(m.mark_point) desc ;
+  SELECT 
+    s.student_ID,
+    s.student_name,
+    s.address,
+    s.phone,
+    s.`status`,
+    s.class_ID,
+    AVG(m.mark_point) AS diem_thi_trung_binh
+FROM
+    student s
+        INNER JOIN
+    mark m ON s.student_ID = m.student_ID
+GROUP BY s.student_ID
+ORDER BY AVG(m.mark_point) DESC;
 
  
   
